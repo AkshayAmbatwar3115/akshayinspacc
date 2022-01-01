436ccddd422ce5422d26a2657350844f0cf16743
Akshayambatwar <61134256+Akshayambatwar@users.noreply.github.com>
2022-01-01 10:07:32 +0530

    first commit

---
+++ helpers/listingSEO.js
@@ -0,0 +1,50 @@
+export const listingSEO = [
+  {
+    route: "/disinfection",
+    title:
+      "Best Disinfection and Sanitization services for Residential Societies, Corporates & Industries | Inspacco",
+  },
+  {
+    route: "/regular-services",
+    title:
+      "Regular and Cleaning services at affordable rates for Residential Societies, Corporates & Industries | Insapcco",
+  },
+  {
+    route: "/repairs",
+    title:
+      "Get Repairs done for Home, Residential Societies & Corporates | Inspacco",
+  },
+  {
+    route: "/maintenance",
+    title:
+      "Get Maintenance and AMC services for Residential Societies, Corporates & Industries | Inspacco",
+  },
+  {
+    route: "/inspection",
+    title:
+      "Book Quality Inspection services for Home, Residential Societies and other Properties at affordable rates | Inspacco",
+  },
+  {
+    route: "/interiors",
+    title:
+      "Best Interior Design Solutions and Services for Home, Corporates, Commercial spaces | Inspacco",
+  },
+  {
+    route: "/fabrication",
+    title:
+      "Fabrication Services for Societies & Industries at affordable rates in India | Inspacco",
+  },
+  {
+    route: "/civil-work",
+    title:
+      "Civil Work and Construction related service providers in India | Inspacco",
+  },
+    {
+      route: "/Water-Proofing",
+      title: "Water leakage detection and Waterproofing service providers for Homes, Residential and Commercial spaces | Inspacco",
+    },
+  //   {
+  //     route: "/",
+  //     title: "Improvement service providers for Home, Residential Societies and Commercial spaces | Inspacco",
+  //   },
+];
